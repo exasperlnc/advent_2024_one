4c526bda9742417adfd33c1f5863b85f7d71b523
Logan Cole <emtlogancole@gmail.com>
2025-09-08 16:56:09 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module fakesite.com
+
+go 1.24.6
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	
+}
+
+// pull the lists from the file
+
+// sort the lists 
+
+// go through the lists and compare the indexes, tracking the difference total 
+
+// return that total 
+
